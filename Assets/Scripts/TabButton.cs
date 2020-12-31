c3276b8066c8c504d0c54e8da7f8519cfdb9c59b
gruby1265 <58722490+gruby1265@users.noreply.github.com>
2020-12-31 14:57:17 +0100

    Tab system
    To co miałem zrobić do deadlinu

---
+++ Assets/Scripts/TabButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public TabGroup tabGroup;
+
+    public Image backgroud;
+
+    
+
+    void Start()
+    {
+        backgroud = GetComponent<Image>();
+        tabGroup.Subscribe(this);
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.TabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.TabExit(this);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.TabSelected(this);
+    }
+}
---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    
+    List<TabButton> tabButtons;
+    public Color tabIdle;
+    public Color tabHover;
+    public Color tabActive;
+    TabButton selectedTab;
+    public List<GameObject> pages;
+
+    public void Subscribe(TabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+        }
+        tabButtons.Add(button);
+    }
+
+    public void TabEnter(TabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.backgroud.color = tabHover;
+        }
+
+    }
+    public void TabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+    public void TabSelected(TabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.backgroud.color = tabActive;
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < pages.Count; i++)
+        {
+            if (i == index)
+            {
+                pages[i].SetActive(true);
+            }
+            else
+            {
+                pages[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs()
+    {
+        foreach(TabButton button in tabButtons)
+        {
+            if(selectedTab!=null && button == selectedTab) { continue; }
+            button.backgroud.color = tabIdle;
+        }
+    }
+}
