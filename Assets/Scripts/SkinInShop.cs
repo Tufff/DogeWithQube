f6416725794735157bc240a335761ee9d169e0bf
gruby1265 <58722490+gruby1265@users.noreply.github.com>
2021-01-08 23:53:00 +0100

    początek skin shopa

---
+++ Assets/Scripts/SkinInShop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinInShop : MonoBehaviour
+{
+    public SSkinInfo skinInfo;
+    public Image skinImage;
+
+    public bool isSkinUnlocked = false;
+
+    private void Awake()
+    {
+        skinImage.sprite = skinInfo.skinSprite;
+    }
+
+    public void OnButtonPress()
+    {
+        if (isSkinUnlocked)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ScribtableObjects/SSkinInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable, CreateAssetMenu(fileName = "New Skin", menuName = "Create New Skin")]
+public class SSkinInfo : ScriptableObject
+{
+    public string skinID;
+    public Sprite skinSprite;
+    public int skinPrice;
+}
