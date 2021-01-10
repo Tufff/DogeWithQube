48817a544f0b3dadd0966000ef3da3d40f566d81
gruby1265 <58722490+gruby1265@users.noreply.github.com>
2021-01-10 18:13:08 +0100

    nic

---
+++ Assets/Scripts/ButtonControler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControler : MonoBehaviour
+{
+    public void ResetPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
--- Assets/Scripts/SkinInShop.cs
+++ Assets/Scripts/SkinInShop.cs
@@ -2,28 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SkinInShop : MonoBehaviour
 {
     public SSkinInfo skinInfo;
     public Image skinImage;
+    public TextMeshProUGUI buttonText;
+    //GameObject player;
 
     public bool isSkinUnlocked = false;
 
     private void Awake()
     {
         skinImage.sprite = skinInfo.skinSprite;
+        IsSkinUnlocked();
+    }
+
+    private void Start()
+    {
+        //player = GameObject.Find("Player");
     }
 
     public void OnButtonPress()
     {
         if (isSkinUnlocked)
         {
-
+            
         }
         else
         {
+            if (true)
+            {
+                PlayerPrefs.SetInt(skinInfo.skinID.ToString(), 1);
+                IsSkinUnlocked();
+            }
+        }
+    }
 
+    void IsSkinUnlocked()
+    {
+        if(PlayerPrefs.GetInt(skinInfo.skinID.ToString()) == 1)
+        {
+            isSkinUnlocked = true;
+            buttonText.text = "Equip";
         }
     }
 }
